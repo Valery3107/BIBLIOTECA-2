ee74ecc92efe64a659e65731dafa3974ee950098
Sofibett1214 <siblanquicet@ucundinamarca.edu.co>
2025-04-09 16:49:35 -0500

    se segmento el codigo

---
+++ main.py
@@ -0,0 +1,7 @@
+from clases import Biblioteca
+
+
+if __name__ == "__main__":
+    biblioteca = Biblioteca()
+    print("¡Bienvenido al Sistema de Gestión de Biblioteca!")
+    biblioteca.menu_principal()
